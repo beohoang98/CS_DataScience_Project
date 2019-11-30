edf857a29104c2790ee60b3006eb15a4868ec44f
beohoang98 <beohoang98@users.noreply.github.com>
2019-11-30 17:37:32 +0700

    crawl meta

--- crawler/tiki.py
+++ crawler/tiki.py
@@ -1,33 +1,48 @@
 from requests_html import HTMLSession
 import pandas as pd
+import re
 
 def get_products(html):
     products_elements = html.find('.product-item')
-    product_list = []
+    product_list = list()
 
     for product in products_elements:
         title = product.attrs['data-title']
         price = product.attrs['data-price']
         brand = product.attrs['data-brand']
         product_list.append({
-            title,
-            brand,
-            price,
+            'title': title,
+            'brand': brand,
+            'price': price,
         })
 
     return list(product_list)
 
 def main():
-    urls = [
-        "https://tiki.vn/laptop/c8095"
-    ]
+    url = "https://tiki.vn/laptop/c8095"
     session = HTMLSession()
-    response = session.get(urls[0], allow_redirects=True)
+    products = list()
+    page = 1
+    total = 1
 
-    products = get_products(response.html)
+    while True:
+        response = session.get(url, params={'page': page}, allow_redirects=True)
+        if not response.ok:
+            break;
 
-    df = pd.DataFrame.from_records(products, columns=['title', 'brand', 'price'])
-    df.to_csv('tiki.csv', sep=',')
+        total_text = response.html.find('.product-box [name=results-count]', first=True).text
+        total_text_number = re.findall(r'\d+', total_text)
+        if (len(total_text_number)):
+            total = int(total_text_number[0])
+
+        products += get_products(response.html)
+        print(f"Crawled page {page}")
+        page += 1
+        if (len(products) >= total):
+            break;
+
+    df = pd.DataFrame(products, columns=['title', 'brand', 'price'])
+    df.to_csv('tiki_meta.csv', sep='\t')
 
 if __name__ == "__main__":
     main()
