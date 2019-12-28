468c98eb6f8ff6747d745559bc7fc92fa4193671
beohoang98 <beohoang98@users.noreply.github.com>
2019-12-28 22:12:24 +0700

    test

--- crawler/preprocess/tgdd.py
+++ crawler/preprocess/tgdd.py
@@ -6,3 +6,6 @@ def TGDDFillNAN():
     fill_price = make_column_transformer(
         (['price'], SimpleImputer(strategy='mean')),
     )
+    fill_keyboard_light = make_column_transformer(
+        (['keyboard_light'], SimpleImputer(strategy='constant', fill_value=0))
+    )
